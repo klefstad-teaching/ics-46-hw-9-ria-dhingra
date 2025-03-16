33a46580e3c494c53697a6132f8279f93ded8657
ria-dhingra <dhingrr1@uci.edu>
2025-03-16 04:38:20 +0000

    debugging printpath

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -45,7 +45,7 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 }
 
 void print_path(const vector<int>& v, int total) {
-    if (v.empty()) {
+    if (total == INF) {
         cout << "No path found." << endl;
         return;
     }
